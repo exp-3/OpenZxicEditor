5d9324b0cf7235e9ae72af535326ed0a5fd35e30
Keva Cremin <415411@nubes.fun>
2024-08-10 00:15:16 +0800

    fix unpack & update installer

--- installers/install_apt.sh
+++ installers/install_apt.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+get_script_dir() {
+    local script_path="$0"
+    if [ "${script_path:0:1}" != "/" ]; then
+        script_path="$(command -v readlink >/dev/null 2>&1 && { readlink -f "$script_path" || :; } || { realpath "$script_path" || :; } || echo "$PWD/$script_path")"
+    fi
+    local script_dir="$(dirname "$script_path")"
+    echo "$script_dir"
+}
+cd "$(get_script_dir)"
 echo
-echo ===== 获取高权限...
+echo ===== 获取根权限...
 sudo echo 提权成功!
 cd ../
 echo
@@ -13,6 +22,7 @@ sudo apt install -y lua5.4 python3 python3-pip pipx
 echo
 echo ===== 安装依赖项...
 sudo apt install -y coreutils curl squashfs-tools mtd-utils
+pipx reinstall jefferson
 pipx install jefferson
 echo
 echo ===== 设置可执行...
@@ -21,4 +31,5 @@ sudo chmod +x *-image
 echo
 echo ===== 环境配置完成!
 read -p "按ENTER键退出..." any_key
+clear
 exit
